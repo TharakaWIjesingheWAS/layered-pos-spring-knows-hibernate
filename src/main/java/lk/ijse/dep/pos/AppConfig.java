961754b7eac32e540c2be87a96e593c0ac74c41f
Tharaka Wijesinghe <stwijasinghe@gmail.com>
2020-09-04 19:45:15 +0530

    initial commit

---
+++ src/main/java/lk/ijse/dep/pos/AppConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep.pos;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@ComponentScan("lk.ijse.dep.pos")
+@Configuration
+@Import(HibernateConfig.class)
+
+public class AppConfig {
+}
---
+++ src/main/java/lk/ijse/dep/pos/dao/SuperDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep.pos.dao;
+
+import org.hibernate.Session;
+
+public interface SuperDAO {
+    public Session getSession();
+}
